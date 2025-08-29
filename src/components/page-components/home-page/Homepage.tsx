9f5c174bff301528254393ae1026b6b718143f26
sijan sakya <shreejan.shakya@alpastechnology.com>
2025-08-29 10:58:39 +0545

    update:text

--- src/components/page-components/home-page/Homepage.tsx
+++ src/components/page-components/home-page/Homepage.tsx
@@ -5,7 +5,7 @@ import ServiceList from './service-list/ServiceList';
 const Homepage = () => {
   return (
     <>
-      <div className="flex flex-col lg:h-[calc(100vh-11rem)] h-[calc(100vh-4rem)] p-4 text-white">
+      <div className="flex flex-col lg:h-[calc(100vh-12rem)] h-[calc(100vh-4rem)] p-4 text-white">
         <div className="flex-1 grid lg:grid-cols-4 grid-cols-1 gap-4 overflow-auto min-h-0">
           <ServiceList />
           <EmployeeList />
